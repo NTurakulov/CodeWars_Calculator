3ca7f1869a119b9e1ff18c288484a60667e6f425
Nozim <n.turakulov@outlook.com>
2022-07-12 17:23:30 +0500

    [T/+] Tests for parentheses added

---
+++ Tests/BracesTests.cs
@@ -0,0 +1,65 @@
+using Calculator;
+
+namespace Tests;
+
+public class ParenthesesTests
+{
+    // ============================= NESTING WITH PARENTHESES =============================
+
+    [Fact]
+    public void SimpleValueInParentheses()
+    {
+        var calc = new XCalculator();
+        var expression = "(1)";
+
+        var actual = calc.Evaluate(expression);
+
+        Assert.Equal(1, actual);
+    }
+
+    [Fact]
+    public void SimpleNegativeValueInParentheses()
+    {
+        var calc = new XCalculator();
+        var expression = "(-2)";
+
+        var actual = calc.Evaluate(expression);
+
+        Assert.Equal(-2, actual);
+    }
+
+    [Fact]
+    public void ValueInNestedParentheses()
+    {
+        var calc = new XCalculator();
+        var expression = "(((5)))";
+
+        var actual = calc.Evaluate(expression);
+
+        Assert.Equal(5, actual);
+    }
+
+    [Fact]
+    public void SimpleExpressionInParentheses()
+    {
+        var calc = new XCalculator();
+        var expression = "(1 + 1)";
+
+        var actual = calc.Evaluate(expression);
+
+        Assert.Equal(2, actual);
+    }
+
+    // ============================= CHANGE OPERATION PRIORITY WITH PARENTHESES =============================
+
+    [Fact]
+    public void ParenthesesPromotesOperationPriority()
+    {
+        var calc = new XCalculator();
+        var expression = "(2 + 2) * 2";
+
+        var actual = calc.Evaluate(expression);
+
+        Assert.Equal(8, actual);
+    }
+}
